8ca64356742ae0ac1c93d8b939a945c44522d8ef
Madkita <abubakaryagob@gmail.com>
2018-05-29 15:31:08 +0800

    initial upload

---
+++ README.md
@@ -0,0 +1,28 @@
+## Splash Creator
+
+#### Requirements:
+
+- A *nix system
+- python2
+- zip and unzip packages
+- python2 IOstring and pillow
+
+#### how to use:
+
+	- copy the photos you want for logo, fastboot and corrupt to pics folder
+	- copy device header file to bin and rename as header_device.img
+	- edit create.sh and change first line to your device codename(should match the header image as well)
+	- from a terminal after cd in to the directory and  run ./create.sh
+	- flash and try
+
+
+ 
+
+
+
+
+
+
+##### credit:
+- codeaurora for python script
+- Gokul NC 
---
+++ create.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+device=Vince
+echo "$device Splash Image Maker"
+echo "-----------------------------"
+width=1080
+height=2160
+echo "creating logo.img..."
+payloadLimit1=100864
+python2 bin/logo_gen.py pics/logo.png $payload_limit1
+mv output.img pic1.img
+echo "creating fastboot.img..."
+payloadLimit2=613888
+python2 bin/logo_gen.py pics/fastboot.png $payload_limit2
+mv output.img pic2.img
+payloadLimit3=101888
+python2 bin/logo_gen.py pics/logo.png $payload_limit3
+mv output.img pic3.img
+echo "creating corrupt.img..."
+payloadLimit4=0
+python2 bin/logo_gen.py pics/logo.png $payload_limit4
+mv output.img pic4.img
+echo "compressing images into one..."
+cat bin/header_"$device".img pic*.img   > splash.img
+rm pic*.img
+if [ -e output/flashable/splash.img ]; then
+    echo "removing old splash image..."
+    rm output/flashable/splash.img
+fi
+mv splash.img output/flashable
+cd output/flashable
+echo "creating flashable zip..."
+if [ -e $device"_Splash.zip" ]; then
+    echo "removing old flashable zip..."
+    rm $device"_Splash.zip"
+fi
+zip -r "$device"_Splash.zip META-INF/* splash.img
+cd -
+echo "flashable zip created as output/"$device"_flashable.zip"
